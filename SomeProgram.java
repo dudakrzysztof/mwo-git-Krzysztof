8dae97b71e5d1f19f78eadb1c453dbe7e8f5762e
dudakrzysztof <60036072+dudakrzysztof@users.noreply.github.com>
2020-01-31 21:07:34 +0100

    Delete SomeProgram.java

--- SomeProgram.java
+++
@@ -1,8 +0,0 @@
-public class SomeProgram { 
-    public static void main(String[] args) {
-      System.out.println("We are learning to use Git."); 
-	  System.out.println("We will be Git experts, soon.");
-    }
-public void costam()
-system.out.println9"coś tam")
-	} 
